45ef1259cea170524d8614e7914c904ed6df4590
Marcello Lamonaca <marcello.lamonaca@gmail.com>
2020-08-06 20:05:24 +0200

    improve code clarity

--- tic_tac_toe_game.py
+++ tic_tac_toe_game.py
@@ -1,6 +1,7 @@
-"""
-A game of Tic Tac Toe
-"""
+""" A game of Tic Tac Toe """
+
+EMPTY = " "
+
 
 def display_board(pad):
     print(f'|  {pad[1]}  |  {pad[2]}  |  {pad[3]}  |')
@@ -20,7 +21,7 @@ def put_0(pad, pad_pos):
     return pad
 
 
-# aks which player between X and O goes first
+# ask which player between X and O goes first
 def ask_starting_player():
     print('First player decides which symbol to use.')
     while True:
@@ -37,18 +38,18 @@ def ask_starting_player():
             print('ERROE: player non valido\n')
 
 
-# asks where tu put the symbol and check if that position is avaiable
+# asks where to put the symbol and check if that position is avaiable
 def ask_position(pad):
     while True:
         pos = input('Where do you want to put your symbol?: ')
-        print()
-        # checks if input is an integer
+
         try:
-            pos = int(pos)
-            # checks if the input is a pad position
+            pos = int(pos)  # check if input is an integer
+
+            # check if the input is a pad position
             if pos in range(1, 10):
-                # checks if the pad position is empty
-                if pad[pos] == ' ':
+                # check if the pad position is EMPTY
+                if pad[pos] == EMPTY:
                     return pos
                 else:
                     print('ERROE: posizione già occupata')
@@ -75,57 +76,58 @@ def check_victory(pad):
     # victory cases: 123, 456, 789, 147, 258, 369, 159, 753
     is_victory = False
 
-    if pad[1] == pad[2] == pad[3] and pad[1] != ' ' and pad[2] != ' ' and pad[3] != ' ':
+    if pad[1] == pad[2] == pad[3] != EMPTY:
         is_victory = True
-    elif pad[4] == pad[5] == pad[6] and pad[4] != ' ' and pad[5] != ' ' and pad[6] != ' ':
+    elif pad[4] == pad[5] == pad[6] != EMPTY:
         is_victory = True
-    elif pad[7] == pad[8] == pad[9] and pad[7] != ' ' and pad[8] != ' ' and pad[9] != ' ':
+    elif pad[7] == pad[8] == pad[9] != EMPTY:
         is_victory = True
-    elif pad[1] == pad[4] == pad[7] and pad[1] != ' ' and pad[4] != ' ' and pad[7] != ' ':
+    elif pad[1] == pad[4] == pad[7] != EMPTY:
         is_victory = True
-    elif pad[2] == pad[5] == pad[8] and pad[2] != ' ' and pad[5] != ' ' and pad[8] != ' ':
+    elif pad[2] == pad[5] == pad[8] != EMPTY:
         is_victory = True
-    elif pad[3] == pad[6] == pad[9] and pad[3] != ' ' and pad[6] != ' ' and pad[9] != ' ':
+    elif pad[3] == pad[6] == pad[9] != EMPTY:
         is_victory = True
-    elif pad[1] == pad[5] == pad[9] and pad[1] != ' ' and pad[5] != ' ' and pad[9] != ' ':
+    elif pad[1] == pad[5] == pad[9] != EMPTY:
         is_victory = True
-    elif pad[7] == pad[5] == pad[3] and pad[7] != ' ' and pad[5] != ' ' and pad[3] != ' ':
+    elif pad[7] == pad[5] == pad[3] != EMPTY:
         is_victory = True
-    elif pad[1] == pad[2] == pad[3] and pad[1] != ' ' and pad[2] != ' ' and pad[3] != ' ':
+    elif pad[1] == pad[2] == pad[3] != EMPTY:
         is_victory = True
     return is_victory
 
 
 def game():
-
-    # starting sequence
-    turn = 0  # starting turn, 0 = O, 1 = X
     turn_counter = 0
-    empty = ' '
-    # empties the pad at the start of the game
-    pad = {1: empty, 2: empty, 3: empty, 4: empty, 5: empty, 6: empty, 7: empty, 8: empty, 9: empty}
+
+    # empty the pad at the start of the game
+    pad = {1: EMPTY, 2: EMPTY, 3: EMPTY, 4: EMPTY, 5: EMPTY, 6: EMPTY, 7: EMPTY, 8: EMPTY, 9: EMPTY}
+
+    # turns: 0 = O, 1 = X
     turn = ask_starting_player()
     display_board(pad)
 
-    do_continue = True
-    while do_continue:
-        # game sequence
+    # game loop
+    while True:
         pos = ask_position(pad)
         if turn == 0:
             pad = put_0(pad, pos)
         elif turn == 1:
             pad = put_x(pad, pos)
+
         display_board(pad)
 
-        turn, turn_counter = switch_player(turn, turn_counter)
-        # check ending sequence
+        turn, turn_counter = switch_player(turn, turn_counter)  # update turn info
+
+        # check victory
         is_victory = check_victory(pad)
         if turn_counter == 9 and not is_victory:
             print("---It's a tie!---".upper())
-            do_continue = False
+            break
         elif is_victory:
             print("---You won!---".upper())
-            do_continue = False
+            break
+
 
 if __name__ == "__main__":
     game()
